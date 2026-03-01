3b6b1b2d201b44ed62600f7c6cf7ef1912efb8a1
kkkaishao <skkfighting@gmail.com>
2026-03-01 23:47:43 +0800

    Use upstream registerAllExtensions function

--- tools/allo-lsp-server/allo-lsp-server.cpp
+++ tools/allo-lsp-server/allo-lsp-server.cpp
@@ -1,11 +1,14 @@
-#include "InitAllDialects.h"
 #include "allo/IR/AlloOps.h"
+#include "mlir/InitAllDialects.h"
+#include "mlir/InitAllExtensions.h"
 #include "mlir/Tools/mlir-lsp-server/MlirLspServerMain.h"
 
 using namespace mlir;
 
 int main(int argc, char **argv) {
   DialectRegistry registry;
-  allo::registerAllDialects(registry);
+  registerAllDialects(registry);
+  registry.insert<allo::AlloDialect>();
+  registerAllExtensions(registry);
   return failed(MlirLspServerMain(argc, argv, registry));
 }
--- tools/allo-opt/allo-opt.cpp
+++ tools/allo-opt/allo-opt.cpp
@@ -1,5 +1,6 @@
-#include "InitAllDialects.h"
 #include "allo/IR/AlloOps.h"
+#include "mlir/InitAllDialects.h"
+#include "mlir/InitAllExtensions.h"
 #include "mlir/InitAllPasses.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
 
@@ -7,7 +8,9 @@ using namespace mlir;
 
 int main(int argc, char **argv) {
   DialectRegistry registry;
-  allo::registerAllDialects(registry);
+  registerAllDialects(registry);
+  registry.insert<allo::AlloDialect>();
+  registerAllExtensions(registry);
   registerAllPasses();
   return asMainReturnCode(
       MlirOptMain(argc, argv, "Allo optimization driver\n", registry));
