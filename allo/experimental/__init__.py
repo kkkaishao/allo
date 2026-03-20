eac2f3d0c1381f1eceb667451d0dbcfceadb8743
kkkaishao <skkfighting@gmail.com>
2026-03-20 12:35:35 +0800

    [Frontend] Simplify package imports

--- allo/experimental/__init__.py
+++ allo/experimental/__init__.py
@@ -1,4 +1,6 @@
 # Copyright Allo authors. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-from .core import *
+from .core.kernel import kernel, accelerator, consteval
+from .compiler.codegen import compile
+from .dsl import *
--- allo/experimental/compiler/__init__.py
+++ allo/experimental/compiler/__init__.py
@@ -1,4 +1,2 @@
 # Copyright Allo authors. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
-
-from .codegen import compile
