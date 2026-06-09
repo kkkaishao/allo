4b86a8f8bd2a63c4dc84437e4e7e1b674eb4701c
kaishao <kaishao0582@gmail.com>
2026-06-09 13:04:31 +0800

    [Chore] Disable Legacy Auto Import and Clean Files

--- allo/__init__.py
+++ allo/__init__.py
@@ -2,12 +2,12 @@
 # SPDX-License-Identifier: Apache-2.0
 # pylint: disable=redefined-builtin
 
-from . import frontend, backend, ir, passes, library, _mlir
-from .customize import customize, Partition
-from .backend.llvm import invoke_mlir_parser, LLVMModule
-from .backend.hls import HLSModule
-from .backend.ip import IPModule
-from .dsl import *
-from .template import *
-from .verify import verify
-from .memory import Memory, Layout
+# from . import frontend, backend, ir, passes, library, _mlir
+# from .customize import customize, Partition
+# from .backend.llvm import invoke_mlir_parser, LLVMModule
+# from .backend.hls import HLSModule
+# from .backend.ip import IPModule
+# from .dsl import *
+# from .template import *
+# from .verify import verify
+# from .memory import Memory, Layout
